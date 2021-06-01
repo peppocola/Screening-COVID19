1ea891f7caf5c8ca51f1f380d8557c8d5aa80319
loreloc <lorenzoloconte@outlook.it>
2021-06-02 00:04:28 +0200

    added images dimensions histogram plot

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from collections import defaultdict
 from PIL import Image as pil
 from PIL import ImageOps as pilops
 from preprocessing import image_preprocess
@@ -19,7 +20,7 @@ def save_visualizations(filepath, identifier, output_dir='visualization'):
     # Obtain the filenames
     original_filename = os.path.join(output_dir, identifier + '.png')
     cropped_filename = os.path.join(output_dir, identifier + '-cropped.png')
-    histogram_filename = os.path.join(output_dir, identifier + '-histogram.png')
+    histogram_filename = os.path.join(output_dir, identifier + '-histogram.pdf')
     equalized_filename = os.path.join(output_dir, identifier + '-equalized.png')
 
     with pil.open(filepath) as img:
@@ -43,18 +44,38 @@ def save_visualizations(filepath, identifier, output_dir='visualization'):
         equalized_img.save(equalized_filename)
 
         # Save the histogram figure
-        xticks = np.arange(9) * 32
+        xticks = np.arange(5) * 64
         xticks[-1] = 255
         yticks = np.arange(6) / 5.0
-        plt.figure(figsize=(4, 4))
+        plt.figure(figsize=(3, 3))
         plt.xlim([0, 255])
         plt.ylim([0.0, 1.0])
         plt.xticks(xticks)
         plt.yticks(yticks)
-        plt.subplots_adjust(top=0.9, bottom=0.1, right=0.9, left=0.1)
         plt.plot(cumulative_histogram, linewidth=2)
-        plt.savefig(histogram_filename, dpi=112)
+        plt.tight_layout()
+        plt.savefig(histogram_filename)
         plt.clf()
+
+
+def save_dimensions_histogram(hist, output_dir='visualization'):
+    # Check the presence of the output directory
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+
+    # Obtain the filenames
+    filename = os.path.join(output_dir, 'dataset-dimensions.pdf')
+
+    # Plot the bar diagram of the image dimensions frequencies
+    dims, freqs = zip(*hist)
+    dims = list(map(lambda x: '{}x{}'.format(x[0], x[1]), dims))
+    plt.figure(figsize=(6, 4))
+    plt.bar(dims, freqs, log=True, width=0.6)
+    plt.xticks(rotation='vertical')
+    plt.ylabel('number of examples')
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.clf()
 
 
 if __name__ == '__main__':
@@ -68,6 +89,7 @@ if __name__ == '__main__':
         names=['patient_id', 'filename', 'class', 'data_source'],
         converters={'class': lambda c: 1 if c == 'positive' else 0}
     )
+
     negative_df = df[df['class'] == 0]
     positive_df = df[df['class'] == 1]
 
@@ -85,3 +107,17 @@ if __name__ == '__main__':
     for idx, filename in enumerate(positive_df['filename']):
         filepath = os.path.join(IMAGES_PATH, filename)
         save_visualizations(filepath, 'cxr2-id{}-positive'.format(idx))
+
+    # Compute the image dimensions histogram
+    histogram = defaultdict(int)
+    for filename in df['filename']:
+        filepath = os.path.join(IMAGES_PATH, filename)
+        with pil.open(filepath) as img:
+            histogram[img.size] += 1
+
+    # Save the image dimensions histogram
+    histogram = list(sorted(
+        filter(lambda x: x[1] >= 5, histogram.items()),
+        key=lambda x: x[1], reverse=True
+    ))
+    save_dimensions_histogram(histogram)
